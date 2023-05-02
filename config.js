776a372cbec6bae93e377e8372aaf325cb1fe356
McDonald Namba <nambamcdonald@gmail.com>
2023-05-02 11:03:33 +0200

    installed required packages

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+const firebase = require('firebase')
+
 const firebaseConfig = {
   apiKey: "AIzaSyBMmICRl5ODHRQPDLL7H__1DycANK9Y25w",
   authDomain: "the-kyc-files.firebaseapp.com",
@@ -7,3 +9,6 @@ const firebaseConfig = {
   appId: "1:385618809544:web:6d925c7d6a9772f569cbfd",
   measurementId: "G-TJBRP10HSD"
 };
+
+firebase.initializeApp(firebaseConfig)
+const db = firebase.firestore()
