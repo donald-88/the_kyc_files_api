459279d0d4be181cb6cb10dde02827c36389297a
McDonald Namba <nambamcdonald@gmail.com>
2023-05-04 20:56:24 +0200

    posting data

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,35 @@
 const functions = require("firebase-functions");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const admin = require("firebase-admin");
+
+const serviceAccount = require("./serviceAccountKeys.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors({origin: true}));
+
+// routes
+app.get("/", (req, res) => {
+  return res.status(200).send("Hello World!");
+});
+
+// Create - post
+
+
+// Read - get
+
+
+// Update - put
+
+
+// Delete - delete
+
+
+// export the api to firebase cloud functions
+exports.app = functions.https.onRequest(app);
